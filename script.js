5df9f6326b1e4541d927f468a77ba1e2418c7197
Frederic <83355061+Gyaku200@users.noreply.github.com>
2022-10-19 14:26:14 +0800

    Added main files

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(()=>{
+    
+    $("#btn-logout").on("click", ()=>{
+        window.location.href = "login.html";
+    }); 
+
+    $("#btn-home").on("click", ()=>{
+        window.location.href = "home.html";
+    });
+
+    $("#btn-option").on("click", ()=>{
+        //Open Modal
+    });
+
+    $("#btn-back").on("click", ()=>{
+        window.location.href = "home.html";
+    });
+
+    $(".cb-head").on("change", ()=>{
+        if($(".cb-head").prop("checked") == true){
+            $('.cb-item').prop('checked', true);
+        }else{
+            $('.cb-item').prop('checked', false);
+        }
+    });
+
+    $(".btn-profile").on("click", ()=>{
+        window.location.href = "profile.html";
+    });
+    
+    $("#btn-add-employee").on("click",()=>{
+        $("#modal").fadeToggle(200);
+    });
+
+    $("#btn-modal-close").on("click", ()=>{
+        $("#modal").fadeToggle(200);
+    });
+
+    $("#btn-modal-cancel").on("click", ()=>{
+        $("#modal").fadeToggle(200);
+    });
+
+    $("#btn-modal-confirm").on("click", ()=>{
+        $("#modal").fadeToggle(200);
+    });
+
+    $("#btn-profile-back").on("click", ()=>{
+        window.location.href = "home.html";
+    });
+
+});
